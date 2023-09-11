b443243878f9d44b85a6d8e2a60a0525eb848c77
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-09-11 19:11:58 +0200

    fix: correct bills display order

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,11 +20,12 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const billsAntiChrono = (a, b) => ((a.date < b.date) ? 1 : -1)
+  return (data && data.length) ? data.sort(billsAntiChrono).map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +48,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
